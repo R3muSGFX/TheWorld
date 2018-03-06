09ca2ed6987d285aaac3d52c2917a971446554d2
Remus Moisi <remus.r3mus@gmail.com>
2018-03-06 15:59:41 +0200

    added bootstrap content to the pages

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -2,33 +2,17 @@
 
 (function () {
 
-	//var ele = $("#username");
-	//ele.text("bla bla");
-
-	//var main = $("#main");
-	//main.on("mouseenter", function () {
-	//	main.style = "background-color: #888;";
-	//});
-
-	//main.on("mouseleave", function () { 
-	//	main.style = "";
-	//});
-
-	//var menuitems = $("ul.menu li a");
-	//menuitems.on("click", function () {
-	//	var me = $(this);
-	//	alert(me.text());
-	//});
-
 	var $sidebarWrapper = $("#sidebar,#wrapper");
+	var $icon = $("#sidebarToggle i.fa");
 
 	$("#sidebarToggle").on("click", function () {
 		$sidebarWrapper.toggleClass("hide-sidebar");
 		if ($sidebarWrapper.hasClass("hide-sidebar")) {
-			$(this).text("Show sidebar");
+			$icon.removeClass("fa-angle-left");
+			$icon.addClass("fa-angle-right");
 		} else {
-			$(this).text("Hide sidebar");
+			$icon.addClass("fa-angle-left");
+			$icon.removeClass("fa-angle-right");
 		}
 	});
-
 })();
